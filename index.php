5bc181f080f73e54c947eece856d4a7e9347d942
FabricioRiosLeal <fabricioriosleal@gmail.com>
2018-12-16 20:36:07 -0200

    Autoload hcodebr classes

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 
 require_once("vendor/autoload.php");
+//require_once './vendor/sistur/config.php';
+
+use \Hcode\DB\Sql;
 
 $app = new \Slim\Slim();
 
@@ -8,7 +11,11 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$sql = new Hcode\DB\sql();
+
+	$result = $sql->select('SELECT * FROM tb_ordersstatus');
+
+	echo json_encode($result);
 
 });
 
