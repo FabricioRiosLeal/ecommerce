42c650775bc65055f7be3021bd6dae5fc47d5453
FabricioRiosLeal <fabricioriosleal@gmail.com>
2018-12-23 19:42:43 -0200

    Page Admin

---
+++ vendor/hcodebr/php-classes/src/pageAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hcode;
+
+class pageAdmin extends page {
+
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+	{
+
+
+		parent::__construct($opts, $tpl_dir);
+	}
+
+}
+
+
+
+
+?>
