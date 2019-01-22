799ee51f175a30040f52ed9546c480b615f6d7f2
FabricioRiosLeal <fabricioriosleal@gmail.com>
2019-01-22 21:07:38 -0200

    Paginação

--- site.php
+++ site.php
@@ -18,15 +18,30 @@ $app->get('/', function() {
 
 $app->get("/categories/:idcategory", function($idcategory){
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++) {
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+
+	}
 
 	$page = new page();
 
 	$page->setTpl("category", [
 		'category'=>$category->getValues(),
-		'products'=>Product::checklist($category->getproducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 ]);
 	
 });
